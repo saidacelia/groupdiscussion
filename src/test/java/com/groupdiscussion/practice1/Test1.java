c6afdbc0ea95eea99b371e716dfe1fc8a62de783
saidacelia <khelifisaida5@gmail.com>
2023-08-03 20:32:25 -0400

    added an other static method

--- src/test/java/com/groupdiscussion/practice1/Test1.java
+++ src/test/java/com/groupdiscussion/practice1/Test1.java
@@ -5,8 +5,19 @@ public class Test1 {
 	public static int myNumber() {
 		int a = 5;
 		int b = 2;
-		
 		return a+b;
 	}
+	
+	
+	
+	public static void call() {
+		myNumber();	
+	}
+	
+	public void click() {
+		myNumber();
+	}
+	
+
 
 }
